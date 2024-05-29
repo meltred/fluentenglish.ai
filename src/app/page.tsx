1a3ed5095d5f3e428793c52b2e69ce8f2cef9a16
Kunal Singh <kunalsin9h@gmail.com>
2024-05-29 11:39:32 +0530

    back to nextjs support hume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Demo from "@/components/Demo";
 import { fetchAccessToken } from "@humeai/voice";
-import { redirect } from "next/navigation";
-
-export const runtime = "edge";
 
 export default async function Home() {
   const accessToken = await fetchAccessToken({
-    apiKey: String(process.env.HUME_API_KEY),
-    clientSecret: String(process.env.HUME_CLIENT_SECRET),
+    apiKey: process.env.HUME_API_KEY,
+    clientSecret: process.env.HUME_CLIENT_SECRET,
   });
 
   if (!accessToken) {
-    return redirect("https://kunalsin9h.com");
+    throw new Error();
   }
 
   return (
