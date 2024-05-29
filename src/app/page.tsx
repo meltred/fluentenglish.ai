60348220f37573b17d3f1d292d59b5b179c6a783
Kunal Singh <kunalsin9h@gmail.com>
2024-05-29 11:46:57 +0530

    testing 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
 import Demo from "@/components/Demo";
 import { fetchAccessToken } from "@humeai/voice";
 
+export const runtime = "edge";
+
 export default async function Home() {
-  const accessToken = await fetchAccessToken({
-    apiKey: process.env.HUME_API_KEY,
-    clientSecret: process.env.HUME_CLIENT_SECRET,
-  });
+  try {
+    const accessToken = await fetchAccessToken({
+      apiKey: process.env.HUME_API_KEY,
+      clientSecret: process.env.HUME_CLIENT_SECRET,
+    });
 
-  if (!accessToken) {
-    throw new Error();
+    if (!accessToken) {
+      throw new Error();
+    }
+
+    return (
+      <main>
+        <p>Access Token: {accessToken}</p>
+        <Demo humeAccessToken={accessToken} />
+      </main>
+    );
+  } catch (e) {
+    console.log(e);
+
+    return <p>Error: {JSON.stringify(e)}</p>;
   }
-
-  return (
-    <main>
-      <Demo humeAccessToken={accessToken} />
-    </main>
-  );
 }
