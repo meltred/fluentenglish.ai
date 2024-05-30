ce40c11e552697a547f5f456c12745912e9579fe
Kunal Singh <kunalsin9h@gmail.com>
2024-05-30 23:18:04 +0530

    init deepgram

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,58 @@
+import { DeepgramError, createClient } from "@deepgram/sdk";
+import { NextResponse, type NextRequest } from "next/server";
+
+export const revalidate = 0;
+
+export async function GET(request: NextRequest) {
+  // exit early so we don't request 70000000 keys while in devmode
+  // in dev mode, just give existing api key
+  if (process.env.DEEPGRAM_ENV !== "production") {
+    return NextResponse.json({
+      key: process.env.DEEPGRAM_API_KEY ?? "",
+    });
+  }
+
+  // since api key can not be pulic, we have to invalidate it every 60 second
+  const url = request.url;
+
+  const deepgram = createClient(process.env.DEEPGRAM_API_KEY_OWNER_SCOPE ?? "");
+
+  let { result: projectsResult, error: projectsError } =
+    await deepgram.manage.getProjects();
+
+  if (projectsError) {
+    return NextResponse.json(projectsError);
+  }
+
+  const project = projectsResult?.projects[0];
+
+  if (!project) {
+    return NextResponse.json(
+      new DeepgramError(
+        "Cannot find a Deepgram project. Please create a project first."
+      )
+    );
+  }
+
+  let { result: newKeyResult, error: newKeyError } =
+    await deepgram.manage.createProjectKey(project.project_id, {
+      comment: "Production *TEMP API key",
+      scopes: ["usage:write"],
+      tags: ["next.js"],
+      time_to_live_in_seconds: 60,
+    });
+
+  if (newKeyError) {
+    return NextResponse.json(newKeyError);
+  }
+
+  const response = NextResponse.json({ ...newKeyResult, url });
+  response.headers.set("Surrogate-Control", "no-store");
+  response.headers.set(
+    "Cache-Control",
+    "s-maxage=0, no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  response.headers.set("Expires", "0");
+
+  return response;
+}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly HUME_API_KEY: string;
-    readonly HUME_CLIENT_SECRET: string;
+    readonly DEEPGRAM_API_KEY_OWNER_SCOPE: string;
+    readonly DEEPGRAM_ENV: string;
     readonly LOGTAIL_SOURCE_TOKEN: string;
   }
 }
